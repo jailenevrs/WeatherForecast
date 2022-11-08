d5208b3304dcef3b5178deb20578ae24226531a9
Jailene <jaileneveras8@gmail.com>
2022-11-07 23:16:56 -0500

    created addEventListener to recieve lon and lat of user input

---
+++ script.js
@@ -0,0 +1,29 @@
+var button = document.querySelector(".search")
+var inputValue = document.querySelector('.input')
+console.log(inputValue.value)
+
+function getCoordinates(cityName){ 
+fetch('http://api.openweathermap.org/geo/1.0/direct?q='+cityName+'&appid=0f9b2277a81917aa8d1fdf73b2af274e')
+ .then (function (response){
+    return response.json();
+ })-
+ .then(function(data){
+    console.log(data) });
+}
+
+
+button.addEventListener('click',function(){
+   console.log(inputValue.value)
+   getCoordinates(inputValue.value)
+});
+
+
+
+function getForecast(){
+   fetch('api.openweathermap.org/data/2.5/forecast?lat=42&lon=-71&appid=0f9b2277a81917aa8d1fdf73b2af274e')
+   .then (function (response){
+      return response.json();
+   
+   });
+}
+   
