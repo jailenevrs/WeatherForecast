1a89444c92ea6c45824d9841e3d740446980eb56
Jailene <jaileneveras8@gmail.com>
2022-11-10 22:48:55 -0500

    used getElementById to show temperature for the next 5 days

--- script.js
+++ script.js
@@ -41,16 +41,43 @@ button.addEventListener('click',function(){
 function getForecast(lat,lon){
    
    console.log(lat)
-   fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=0f9b2277a81917aa8d1fdf73b2af274e')
+   fetch('https://api.openweathermap.org/data/2.5/forecast?lat='+lat+'&lon='+lon+'&appid=0f9b2277a81917aa8d1fdf73b2af274e&units=imperial')
    .then (function (response){
       return response.json();
    })
    .then(function(data){
      var forecast=get5DaysForecast(data)
-      console.log(forecast) });
+      console.log(forecast) 
+      console.log(forecast[0].main.temp);
+
+      var day1data =forecast[0].main.temp
+      document.getElementById("day1").innerHTML="Temperature: " + day1data   
+
+      var day2data=forecast[1].main.temp
+      document.getElementById("day2").innerHTML= "Temperature: " + day2data
+
+      var day3data=forecast[2].main.temp
+      document.getElementById("day3").innerHTML= "Temperature: " + day3data
+
+      var day4data=forecast[3].main.temp
+      document.getElementById("day4").innerHTML= "Temperature: " + day4data
+   
+      var day5data=forecast[4].main.temp
+      document.getElementById("day5").innerHTML= "Temperature: " + day5data
+   
+
+      console.log(forecast[0].)
+   
+   
+   });
+     // var day1data =forecast[0].main.temp
+     // document.getElementById("day1").innerHTML=day1data    
    }
    
 
   
    
 getForecast()
+
+
+
