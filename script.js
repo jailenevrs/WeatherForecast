e26df16df7a2814b53cc54f810277dacaa77655d
Jailene <jaileneveras8@gmail.com>
2022-11-11 00:13:16 -0500

    addd margins to weather cards and removed buttons

--- script.js
+++ script.js
@@ -55,31 +55,31 @@ function getForecast(lat,lon){
       var day1hum = forecast[0].main.humidity
       var day1temp =forecast[0].main.temp
       document.getElementById("day1").innerHTML="Temperature: " + day1temp  + "  Wind Speed: " + day1wind + " Mph"
-      + " Humidity: " + day1hum 
+      + " Humidity: " + day1hum + "%" 
 
       var day2wind = forecast[1].wind.speed
       var day2hum = forecast[1].main.humidity
       var day2temp=forecast[1].main.temp
       document.getElementById("day2").innerHTML= "Temperature: " + day2temp + "  Wind Speed: " + day2wind + " Mph"
-      + " Humidity: " + day2hum 
+      + " Humidity: " + day2hum + "%"
 
       var day3wind = forecast[2].wind.speed
       var day3hum = forecast[2].main.humidity
       var day3temp=forecast[2].main.temp
       document.getElementById("day3").innerHTML= "Temperature: " + day3temp + "  Wind Speed: " + day3wind + " Mph"
-      + " Humidity: " + day3hum
+      + " Humidity: " + day3hum + "%"
 
       var day4wind = forecast[3].wind.speed
       var day4hum = forecast[3].main.humidity
       var day4temp=forecast[3].main.temp
       document.getElementById("day4").innerHTML= "Temperature: " + day4temp+ "  Wind Speed: " + day4wind + " Mph"
-      + " Humidity: " + day4hum
+      + " Humidity: " + day4hum + "%"
 
       var day5wind = forecast[4].wind.speed
       var day5hum = forecast[4].main.humidity
       var day5temp=forecast[4].main.temp
       document.getElementById("day5").innerHTML= "Temperature: " + day5temp + "  Wind Speed: " + day5wind + " Mph"
-      + " Humidity: " + day5hum
+      + " Humidity: " + day5hum + "%"
    
          console.log(forecast[0].wind.speed)
 
