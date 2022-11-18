687f73b6ea0a00a05031ca569650bb8475e4ef3d
Jailene <jaileneveras8@gmail.com>
2022-11-17 23:39:18 -0500

    Made an img tag and id for every card in the html. Also created a variable for icon and plugged it into the icon url from open weather map

--- script.js
+++ script.js
@@ -54,32 +54,46 @@ function getForecast(lat,lon){
       var day1wind = forecast[0].wind.speed
       var day1hum = forecast[0].main.humidity
       var day1temp =forecast[0].main.temp
+      var icon1 = forecast[0].weather[0].icon
       document.getElementById("day1").innerHTML="Temperature: " + day1temp  + "  Wind Speed: " + day1wind + " Mph"
-      + " Humidity: " + day1hum + "%" 
+      + " Humidity: " + day1hum + "% " 
+      document.getElementById("iconPic1").src = "http://openweathermap.org/img/w/"+icon1+".png";
+     
+
+    
+   
 
       var day2wind = forecast[1].wind.speed
       var day2hum = forecast[1].main.humidity
       var day2temp=forecast[1].main.temp
+      var icon2 = forecast[1].weather[0].icon
       document.getElementById("day2").innerHTML= "Temperature: " + day2temp + "  Wind Speed: " + day2wind + " Mph"
       + " Humidity: " + day2hum + "%"
-
+      document.getElementById("iconPic2").src = "http://openweathermap.org/img/w/"+icon2+".png";
+      
       var day3wind = forecast[2].wind.speed
       var day3hum = forecast[2].main.humidity
       var day3temp=forecast[2].main.temp
+      var icon3 = forecast[2].weather[0].icon
       document.getElementById("day3").innerHTML= "Temperature: " + day3temp + "  Wind Speed: " + day3wind + " Mph"
       + " Humidity: " + day3hum + "%"
+      document.getElementById("iconPic3").src = "http://openweathermap.org/img/w/"+icon3+".png";
 
       var day4wind = forecast[3].wind.speed
       var day4hum = forecast[3].main.humidity
       var day4temp=forecast[3].main.temp
+      var icon4 = forecast[3].weather[0].icon
       document.getElementById("day4").innerHTML= "Temperature: " + day4temp+ "  Wind Speed: " + day4wind + " Mph"
       + " Humidity: " + day4hum + "%"
+      document.getElementById("iconPic4").src = "http://openweathermap.org/img/w/"+icon4+".png";
 
       var day5wind = forecast[4].wind.speed
       var day5hum = forecast[4].main.humidity
       var day5temp=forecast[4].main.temp
+    var icon5= forecast[4].weather[0].icon
       document.getElementById("day5").innerHTML= "Temperature: " + day5temp + "  Wind Speed: " + day5wind + " Mph"
       + " Humidity: " + day5hum + "%"
+      document.getElementById("iconPic5").src = "http://openweathermap.org/img/w/"+icon5+".png";
    
          console.log(forecast[0].wind.speed)
 
