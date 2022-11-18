c45426783d2dd2f9898a716c175ffb328a9fb5c2
Jailene <jaileneveras8@gmail.com>
2022-11-18 11:15:21 -0500

    took date for next five days and printed to html. Used split method to get some parts of date

--- script.js
+++ script.js
@@ -55,10 +55,13 @@ function getForecast(lat,lon){
       var day1hum = forecast[0].main.humidity
       var day1temp =forecast[0].main.temp
       var icon1 = forecast[0].weather[0].icon
+      var day1Date = forecast[0].dt_txt
+      var split1Date = day1Date.slice(0,10);
+      console.log(split1Date)
       document.getElementById("day1").innerHTML="Temperature: " + day1temp  + "  Wind Speed: " + day1wind + " Mph"
       + " Humidity: " + day1hum + "% " 
       document.getElementById("iconPic1").src = "http://openweathermap.org/img/w/"+icon1+".png";
-     
+     document.getElementById("Date1").innerHTML=split1Date
 
     
    
@@ -67,33 +70,46 @@ function getForecast(lat,lon){
       var day2hum = forecast[1].main.humidity
       var day2temp=forecast[1].main.temp
       var icon2 = forecast[1].weather[0].icon
+      var day2Date = forecast[1].dt_txt
+      var split2Date = day2Date.slice(0,10);
       document.getElementById("day2").innerHTML= "Temperature: " + day2temp + "  Wind Speed: " + day2wind + " Mph"
       + " Humidity: " + day2hum + "%"
       document.getElementById("iconPic2").src = "http://openweathermap.org/img/w/"+icon2+".png";
+      document.getElementById("Date2").innerHTML=split2Date
       
       var day3wind = forecast[2].wind.speed
       var day3hum = forecast[2].main.humidity
       var day3temp=forecast[2].main.temp
       var icon3 = forecast[2].weather[0].icon
+      var day3Date = forecast[2].dt_txt
+      var split3Date = day3Date.slice(0,10);
       document.getElementById("day3").innerHTML= "Temperature: " + day3temp + "  Wind Speed: " + day3wind + " Mph"
       + " Humidity: " + day3hum + "%"
       document.getElementById("iconPic3").src = "http://openweathermap.org/img/w/"+icon3+".png";
+      document.getElementById("Date3").innerHTML=split3Date
+      
 
       var day4wind = forecast[3].wind.speed
       var day4hum = forecast[3].main.humidity
       var day4temp=forecast[3].main.temp
       var icon4 = forecast[3].weather[0].icon
+      var day4Date = forecast[3].dt_txt
+      var split4Date = day4Date.slice(0,10);
       document.getElementById("day4").innerHTML= "Temperature: " + day4temp+ "  Wind Speed: " + day4wind + " Mph"
       + " Humidity: " + day4hum + "%"
       document.getElementById("iconPic4").src = "http://openweathermap.org/img/w/"+icon4+".png";
+      document.getElementById("Date4").innerHTML=split4Date
 
       var day5wind = forecast[4].wind.speed
       var day5hum = forecast[4].main.humidity
       var day5temp=forecast[4].main.temp
     var icon5= forecast[4].weather[0].icon
+    var day5Date = forecast[4].dt_txt
+   var split5Date = day5Date.slice(0,10);
       document.getElementById("day5").innerHTML= "Temperature: " + day5temp + "  Wind Speed: " + day5wind + " Mph"
       + " Humidity: " + day5hum + "%"
       document.getElementById("iconPic5").src = "http://openweathermap.org/img/w/"+icon5+".png";
+      document.getElementById("Date5").innerHTML=split5Date
    
          console.log(forecast[0].wind.speed)
 
@@ -108,6 +124,8 @@ function getForecast(lat,lon){
   
    
 getForecast()
+
+
 
 
 
